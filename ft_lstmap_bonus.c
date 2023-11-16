b8014f23d691f34d85ac268ad55bfffed6ab8189
awahib <awahib@c4r6p7.1337.ma>
2023-11-17 00:00:49 +0100

    2

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: awahib <awahib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 09:45:35 by awahib            #+#    #+#             */
-/*   Updated: 2023/11/14 20:08:21 by awahib           ###   ########.fr       */
+/*   Updated: 2023/11/16 23:38:24 by awahib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 
 	new_lst = NULL;
-	while (lst)
+	if (lst && f && del)
 	{
-		tmp = ft_lstnew(f(lst->content));
-		if (!tmp)
+		while (lst)
 		{
-			ft_lstclear(&new_lst, del);
-			return (NULL);
+			tmp = ft_lstnew(f(lst->content));
+			if (!tmp)
+			{
+				ft_lstclear(&new_lst, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&new_lst, tmp);
+			lst = lst->next;
 		}
-		ft_lstadd_back(&new_lst, tmp);
-		lst = lst->next;
 	}
 	return (new_lst);
 }
