4fb432ea5d210bc54be3173557c73e22d1cb9710
awahib <awahib@c4r4p4.1337.ma>
2023-11-17 21:13:14 +0100

    2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: awahib <awahib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 20:03:20 by awahib            #+#    #+#             */
-/*   Updated: 2023/11/16 22:28:30 by awahib           ###   ########.fr       */
+/*   Updated: 2023/11/17 20:24:02 by awahib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*newstr;
 	size_t	i;
 	size_t	j;
-	size_t	tlen;
+	size_t	t_len;
 
 	if (!s1 && !s2)
 		return (NULL);
 	i = 0;
 	j = 0;
-	tlen = ft_strlen(s1) + ft_strlen(s2);
-	newstr = malloc(sizeof(char) * (tlen + 1));
+	t_len = ft_strlen(s1) + ft_strlen(s2);
+	newstr = malloc(sizeof(char) * (t_len + 1));
 	if (!newstr)
 		return (NULL);
 	while (s1[i])
