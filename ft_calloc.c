cc0fa8c93c6696846f561a87b33bdb433c61ded3
awahib <awahib@c4r4p8.1337.ma>
2023-11-17 18:26:38 +0100

    1

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: awahib <awahib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 18:19:55 by awahib            #+#    #+#             */
-/*   Updated: 2023/11/17 18:15:47 by awahib           ###   ########.fr       */
+/*   Updated: 2023/11/17 18:23:59 by awahib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	*ft_calloc(size_t nitems, size_t size)
 	size_t	buffer;
 
 	ptr = NULL;
-<<<<<<< HEAD
 	buffer = nitems * size;
 	if (nitems && buffer / nitems != size)
 		return (NULL);
@@ -27,19 +26,4 @@ void	*ft_calloc(size_t nitems, size_t size)
 		return (NULL);
 	ft_bzero(ptr, buffer);
 	return (ptr);
-=======
-	if (nitems == 0 || size == 0)
-		return (NULL);
-	if (nitems > 0 && size > 0)
-	{
-		buffer = nitems * size;
-		if (buffer / nitems != size)
-			return (NULL);
-		ptr = malloc(buffer);
-		if (!ptr)
-			return (NULL);
-		ft_bzero(ptr, buffer);
-	}
-	return ((void *)ptr);
->>>>>>> c9633a3243b1a329aae6d14163e0b04cd99596f5
 }
